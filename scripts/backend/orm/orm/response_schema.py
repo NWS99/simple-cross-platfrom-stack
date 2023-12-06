f6d85353ec78ff319b47c1ff608a868cc2ab3674
NWS99 <warmuthniklas@googlemail.com>
2023-12-06 13:05:33 +0100

    created a first response schema setup

---
+++ scripts/backend/orm/orm/response_schema.py
@@ -0,0 +1,10 @@
+"""
+This Module is used to create the pydantic models for the response schema.
+"""
+
+from datetime import datetime
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    pass
