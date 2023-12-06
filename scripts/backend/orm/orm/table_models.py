c7b3ead9115d3d005729df6ac5722d1803d084f1
NWS99 <warmuthniklas@googlemail.com>
2023-12-06 13:06:39 +0100

    created a first table model setup

---
+++ scripts/backend/orm/orm/table_models.py
@@ -0,0 +1,8 @@
+"""
+This Module is used to create the SQL Table Model
+"""
+
+from sqlalchemy import Boolean, Column, ForeignKey, \
+    Integer, String, DateTime
+from orm.database_connection import Base
+
