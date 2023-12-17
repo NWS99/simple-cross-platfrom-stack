fe2e4ecb356e69fc1574fda0d6cb7343106da9b0
NWS99 <warmuthniklas@googlemail.com>
2023-12-17 20:48:49 +0100

    updated nix file to have all dependencies

--- shell.nix
+++ shell.nix
@@ -2,6 +2,20 @@
 let
   python-packages = ps: with ps; [
     flet
+    fastapi
+    pydantic
+    psycopg2
+    sqlalchemy
+    uvicorn
+    buildPythonPackage rec {
+      pname = "fastapi-crudrouter";
+      version = "v0.8.6";
+      src = fetchPypi {
+        inherit pname version;
+        sha256 = "sha256-0aozmQ4Eb5zL4rtNHSFjEynfObUkYlid1PgMDVmRkwY=";
+      };
+      doCheck = false;
+    }
   ];
   my-python = pkgs.python310.withPackages python-packages;
 in my-python.env
